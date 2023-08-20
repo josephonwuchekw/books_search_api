3472ac7a49f50b60da59f5bcc1b3b3b23b96debf
josephonwuchekw <josephonwu0@gmail.com>
2023-08-20 03:24:42 -0400

    books search endpoints

--- api/main.py
+++ api/main.py
@@ -1,17 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from .fetch_books import fetch_books_by_query, fetch_book_by_id
 from mangum import Mangum
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
 
 app = FastAPI()
-google_api_key = os.getenv("GOOGLE_API_KEY")
 
 
 @app.get("/")
-def root():
-    return {"message": google_api_key}
+async def root():
+    return {"message": "API"}
 
+
+@app.get("/api/v1/books/")
+async def get_books(q: str, download_format: str = "",
+                    filter: str = "", start_index: int = 0,
+                    count_per_page: int = 10, print_type: str = 'all', order_by: str = 'relevance'):
+    # fetch books by query
+    query_params = (
+        q,
+        filter,
+        start_index,
+        count_per_page,
+        print_type,
+        download_format == 'epub',
+        order_by
+    )
+    result = fetch_books_by_query(*query_params)
+
+    if 'error' in result.keys():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f'{result["error"]["message"]}')
+    return result
+
+
+@app.get("/api/v1/book/{book_id}")
+async def get_book(book_id):
+    result = fetch_book_by_id(book_id)
+
+    if 'error' in result.keys():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f'{result["error"]["message"]}')
+    return result
 
 handler = Mangum(app=app)
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,7 +4,17 @@ from api.main import app
 client = TestClient(app)
 
 
-def test_root():
+def test_root_returns_json():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()
+
+
+def test_get_books_valid_query():
+    response = client.get("/api/v1/books/?q=mohicans")
+    assert response.status_code == 200
+
+
+def test_get_books_empty_query():
+    response = client.get("/api/v1/books/?q=")
+    assert response.status_code == 400
